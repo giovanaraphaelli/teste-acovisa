7078c0e488d50caaf07b4d77492042b8a2cd05c2
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-06-29 19:26:30 -0300

    add clearForm

--- js/cep.js
+++ js/cep.js
@@ -1,3 +1,10 @@
+const clearForm = () => {
+  document.getElementById('address').value = '';
+  document.getElementById('address-b').value = '';
+  document.getElementById('address-c').value = '';
+  document.getElementById('address-e').value = '';
+};
+
 const fillForm = (address) => {
   document.getElementById('address').value = address.logradouro;
   document.getElementById('address-b').value = address.bairro;
@@ -6,6 +13,7 @@ const fillForm = (address) => {
 };
 
 document.getElementById('cep').addEventListener('focusout', async () => {
+  clearForm();
   const cep = document.getElementById('cep').value;
   const url = `http://viacep.com.br/ws/${cep}/json/`;
 
@@ -16,5 +24,4 @@ document.getElementById('cep').addEventListener('focusout', async () => {
   } else {
     fillForm(address);
   }
-  s;
 });
